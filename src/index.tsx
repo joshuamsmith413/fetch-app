1e8a0ce2aee428e38919fa943ea812237186a457
joshuamsmith413 <joshsmith@joshs-MacBook-Pro.local>
2025-02-10 20:47:10 -0500

    replace relative imports with absolute

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
-import "./index.css";
-import App from "./App";
+import "index.css";
+import App from "App";
 
 const rootElement = document.getElementById("root");
 const queryClient = new QueryClient();
